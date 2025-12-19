7fbf956e1bfb28597426f55f951f6f7e3c5c9547
pram07 <pkumar@skycliffit.com>
2025-12-19 12:20:22 +0530

    apitest

--- obj/Debug/net8.0/AWSWebApplication.AssemblyInfo.cs
+++ obj/Debug/net8.0/AWSWebApplication.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AWSWebApplication")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8f22b755a8fa36108b2bdf267c309a44764efd0f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+11de56ac3a4148ff619169044de98ebab1d6c0ba")]
 [assembly: System.Reflection.AssemblyProductAttribute("AWSWebApplication")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AWSWebApplication")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
