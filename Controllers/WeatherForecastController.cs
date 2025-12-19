b735df46f5a98f91439303a0976830f8e6aee85a
pram07 <pkumar@skycliffit.com>
2025-12-19 15:20:51 +0530

    pramodwebapi

--- Controllers/WeatherForecastController.cs
+++ Controllers/WeatherForecastController.cs
@@ -8,7 +8,7 @@ namespace AWSWebApplication.Controllers
     {
         private static readonly string[] Summaries = new[]
         {
-            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching","Pramod" ,
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching","PramodKumar" ,
         };
 
         private readonly ILogger<WeatherForecastController> _logger;
